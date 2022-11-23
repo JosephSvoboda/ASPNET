b623ca90f76b8c74844212c8ca375ea3a85e1042
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-11-23 15:05:51 -0500

    Made an update product .

--- Testing/Repos/IProductRepository.cs
+++ Testing/Repos/IProductRepository.cs
@@ -7,5 +7,6 @@ namespace Testing.Repos
     {
         public IEnumerable<Product> GetAllProducts();
         public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
     }
 }
